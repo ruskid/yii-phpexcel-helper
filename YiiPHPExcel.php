e39159aa94eaf21f924797dc8d51d37925fb2ad2
Victor Demin <demmbox@gmail.com>
2015-02-10 10:30:52 +0100

    Update YiiPHPExcel.php
    bugfix.

--- YiiPHPExcel.php
+++ YiiPHPExcel.php
@@ -43,7 +43,7 @@ class YiiPHPExcel extends PHPExcel {
      * @return will return file to browser
      */
     public function createExcel($models, $attributes) {
-        $letters = $this->getLetters($models);
+        $letters = $this->getLetters($attributes);
         $this->setFirstRow($models, $attributes, $letters);
         $this->setRows($models, $attributes, $letters);
         $this->sendToBrowser();
