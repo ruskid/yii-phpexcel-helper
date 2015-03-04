a84b6242a79757f2fd57f452639a60f28c471986
Victor Demin <demmbox@gmail.com>
2015-03-04 18:01:25 +0100

    possiblity to change filename

--- YiiPHPExcel.php
+++ YiiPHPExcel.php
@@ -50,7 +50,7 @@ class YiiPHPExcel extends PHPExcel {
      */
     public function writeBlankRow() {
         $rownum = $this->getStartRowNumber();
-        $this->setStartRowNumber(++$rownum);
+        $this->setStartRowNumber( ++$rownum);
     }
 
     /**
@@ -176,16 +176,17 @@ class YiiPHPExcel extends PHPExcel {
             $counter++;
         }
         //After writing the data into excel, set the next start row number.
-        $this->setStartRowNumber( ++$rownum);
+        $this->setStartRowNumber(++$rownum);
     }
 
     /**
      * Will send the constructed excel to browser and end yii application
+     * @param string $filename
      */
-    public function sendToBrowser() {
+    public function sendToBrowser($filename = null) {
         // ** Redirect output to a client’s web browser (Excel2007)
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="gpd.xlsx"');
+        header('Content-Disposition: attachment;filename="' . $filename . '.xlsx"');
         header('Cache-Control: max-age=0');
 
         $objWriter = PHPExcel_IOFactory::createWriter($this, 'Excel2007');
